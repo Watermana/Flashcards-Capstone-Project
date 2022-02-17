4a227ee977757ab1b90a54e1c4c0329481a9466b
Austin Waterman <83792316+Watermana@users.noreply.github.com>
2022-02-17 14:21:54 -0700

    added comments

--- src/Layout/BreadCrumb.js
+++ src/Layout/BreadCrumb.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+// General Component for adding the breadcrumb to other components
+
 export default function BreadCrumb({deck, subname}) {
 
     return (
@@ -22,24 +24,3 @@ export default function BreadCrumb({deck, subname}) {
     </nav>
       );
 }
-
-
-/*
-<nav aria-label="breadcrumb">
-    <ol className="breadcrumb">
-    {!subname ? (
-        <>
-        <li className="breadcrumb-item"><Link to="/"><span className="oi oi-home"></span>Home</Link></li>
-        <li className="breadcrumb-item">{deck.name}</li>
-        </>
-    ) : (
-        <>
-        <li className="breadcrumb-item"><Link to="/"><span className="oi oi-home"></span>Home</Link></li>
-        <li className="breadcrumb-item"><Link to={`/decks/${deck.id}`}>{deck.name}</Link></li>
-        <li className="breadcrumb-item active" aria-current="page">{subname}</li>
-        </>
-    )}
-    </ol>
-    </nav>
-      );
-*/
